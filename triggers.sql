7fdf2c39a5c359f5fecf569e2e4f726018d4600d
brettmccausland <brettmccausland@yahoo.com>
2018-11-19 19:44:16 -0800

    s

--- triggers.sql
+++ triggers.sql
@@ -3,7 +3,7 @@
 -- should not include value for part number and will look like this:
 
 -- Insert into part nyc(supplier ,color ,on hand,descr) Values (0,0,20,’Desc’);
-CREATE SEQUENCE partnumber START 50000;
+CREATE OR REPLACE FUNCTION SEQUENCE partnumber START 50000;
 
 -- 2.Create a procedure that will return next value of the
 -- aforementioned sequence. Use function nextval(’part number seq’)
